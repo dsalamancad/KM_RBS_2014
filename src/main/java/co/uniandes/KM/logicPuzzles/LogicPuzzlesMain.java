9065f8d525802ca93da66266e1d8bfab3b8c406d
Sebastian <sebashepin@gmail.com>
2014-10-04 19:00:43 -0500

    Add a prettier theme

--- src/main/java/co/uniandes/KM/logicPuzzles/LogicPuzzlesMain.java
+++ src/main/java/co/uniandes/KM/logicPuzzles/LogicPuzzlesMain.java
@@ -3,6 +3,8 @@
  */
 package co.uniandes.KM.logicPuzzles;
 
+import javax.swing.UIManager;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -15,54 +17,60 @@ import co.uniandes.KM.logicPuzzles.mundo.LogicPuzzle;
  *
  */
 public class LogicPuzzlesMain {
-	
-	private KieSession kSession;
-	private LogicPuzzle logicPuzzle;
-	
-	public LogicPuzzlesMain(int dimensions, int itemsPerDimension) {
-	    Configuration.DIMENSION_AMOUNT = dimensions;
-	    Configuration.ITEMS_PER_DIMENSION = itemsPerDimension;
-	    
-		//Initialize UI
-		Tablero tablero = new Tablero();
-		tablero.setVisible(true);
-		
-		while(tablero.getLogicPuzzle() == null){
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		//Get backend
-		this.logicPuzzle = tablero.getLogicPuzzle();
-		
-		//Initialize drools
+
+    private KieSession  kSession;
+    private LogicPuzzle logicPuzzle;
+
+    public LogicPuzzlesMain(int dimensions, int itemsPerDimension) {
+        Configuration.DIMENSION_AMOUNT = dimensions;
+        Configuration.ITEMS_PER_DIMENSION = itemsPerDimension;
+
+        // Initialize UI
+        Tablero tablero = new Tablero();
+        tablero.setVisible(true);
+
+        while (tablero.getLogicPuzzle() == null) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Get backend
+        this.logicPuzzle = tablero.getLogicPuzzle();
+
+        // Initialize drools
         try {
-        	initializeDrools();
+            initializeDrools();
         } catch (Throwable t) {
             t.printStackTrace();
             System.exit(0);
         }
-        
-        logicPuzzle.setSession(kSession);
-	}
 
-	private void initializeDrools() throws Throwable {
+        logicPuzzle.setSession(kSession);
+    }
+
+    private void initializeDrools() throws Throwable {
         // load up the knowledge base
         KieServices ks = KieServices.Factory.get();
-	    KieContainer kContainer = ks.getKieClasspathContainer();
-    	kSession = kContainer.newKieSession("ksession-rules");
-	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	    int dimensions = args.length == 2 ? Integer.parseInt(args[0]):3;
-	    int itemsPerDimension = args.length == 2 ? Integer.parseInt(args[1]):4;
-		@SuppressWarnings("unused")
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        kSession = kContainer.newKieSession("ksession-rules");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int dimensions = args.length == 2 ? Integer.parseInt(args[0]) : 3;
+        int itemsPerDimension = args.length == 2 ? Integer.parseInt(args[1]) : 4;
+        try {
+            UIManager.setLookAndFeel("com.alee.laf.WebLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        @SuppressWarnings("unused")
         LogicPuzzlesMain lpm = new LogicPuzzlesMain(dimensions, itemsPerDimension);
-	}
+    }
 
 }
